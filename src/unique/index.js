722c54f973875b675bf551ada23e3c1a93fafd08
wanli.yu <wanli.yu@beibei.com>
2019-07-17 09:43:13 +0800

    add unique

---
+++ src/unique/index.js
@@ -0,0 +1,15 @@
+var array1 = [1, 1, '1', '1'];
+
+var res1 = array1.filter((val,index) => {
+    // indexOf() 方法可返回某个指定的字符串值在字符串中首次出现的位置。
+    return array1.indexOf(val) === index
+})
+
+console.log(res1)
+
+var array1 = [1, 1, '1', '1'];
+var res2 = []
+for(let i = 0;i<array1.length;i++){
+    for (let j=0;j<res2.length;j++){
+    }
+}
